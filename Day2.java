da992d4af547ee5d503755a73b4cf865a680836a
RizkyaAdiwijaya10 <rkyywx@gmail.com>
2023-10-10 07:19:09 +0700

    day2

---
+++ Day2.java
@@ -0,0 +1,11 @@
+package ISC;
+
+public class Day2 {
+    public static void main(String[] args) {
+        String nama = "Rizky adiwijaya";
+        int umur = 20;
+        String prodi = "INFORMATIKA";
+        String NIM = "D0222045";
+        
+    }
+}
