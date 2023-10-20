11c81bc6999a311940c4b25461a6c1a5feda24ed
RizkyAdiwijaya10 <147222885+RizkyAdiwijaya10@users.noreply.github.com>
2023-10-20 23:31:08 +0800

    Add files via upload

---
+++ day12.java
@@ -0,0 +1,16 @@
+package informatic_study_club;
+
+import java.util.ArrayList;
+
+public class day12 {
+
+    public static void main(String[] args) {
+        ArrayList data = new ArrayList();
+        data.add(12);
+        data.add(104.23);
+        data.add(4390f);
+        data.add("rizky");
+        data.add('@');
+        System.out.println(data);
+    }
+}
