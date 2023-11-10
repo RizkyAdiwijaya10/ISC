bc9341344e4cc957675ea99dede5c5191aeb6ea6
RizkyAdiwijaya10 <147222885+RizkyAdiwijaya10@users.noreply.github.com>
2023-11-10 21:48:28 +0800

    Add files via upload

---
+++ Day33.java
@@ -0,0 +1,7 @@
+public class Day33{
+    public static void main (String[] args){
+        int [] dataArray = {1,2,3,4,5,6,7,8,9,10};
+        int i = dataArray.length;
+        System.out.print(dataArray[2]);
+    }
+}
