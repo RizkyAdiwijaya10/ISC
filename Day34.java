4ca94ee952afdc185752bc31e6d36842ff819ee4
RizkyAdiwijaya10 <147222885+RizkyAdiwijaya10@users.noreply.github.com>
2023-11-11 22:51:43 +0800

    Add files via upload

---
+++ Day34.java
@@ -0,0 +1,10 @@
+public class Day34{
+    public static void main(String[] args) {
+        int angkaInteger = 12345;
+        
+        String angkaString = String.valueOf(angkaInteger);
+        
+        System.out.println("Bilangan bulat: " + angkaInteger);
+        System.out.println("Hasil konversi ke string: " + angkaString);
+    }
+}
