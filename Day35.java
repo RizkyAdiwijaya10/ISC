67eb5ffc3248181928ca516a960570b6036cd806
RizkyAdiwijaya10 <147222885+RizkyAdiwijaya10@users.noreply.github.com>
2023-11-12 23:28:28 +0800

    Add files via upload

---
+++ Day35.java
@@ -0,0 +1,9 @@
+public class Day35 {
+    public static void main(String[] args) {
+        int []nilai = {1,2,3,4,5,6,7,8,9,10};
+        
+        for (int i = 0; i < nilai.length; i++) {
+            System.out.println("index["+i+"] = "+nilai[i]);
+        }
+    }
+}
