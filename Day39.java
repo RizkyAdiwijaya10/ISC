9e0c3d6e56ed9f2d4381da2daf8b47959be532fb
RizkyAdiwijaya10 <147222885+RizkyAdiwijaya10@users.noreply.github.com>
2023-11-16 22:51:34 +0800

    Add files via upload

---
+++ Day39.java
@@ -0,0 +1,8 @@
+public class Day39{
+    public static void main(String[] args) {
+        hari("kamis");
+    }
+    public static void hari(String hari){
+        System.out.println("sekarang hari : " + hari);
+    }
+}
