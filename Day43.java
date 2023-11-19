8fd22f564e67a84445f8947cfbaf06c7bddf19c7
RizkyAdiwijaya10 <147222885+RizkyAdiwijaya10@users.noreply.github.com>
2023-11-20 00:11:53 +0800

    Add files via upload

---
+++ Day43.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Day43{
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("nilai : ");
+        int n = scanner.nextInt();
+        
+        if(n % 2 == 0){
+                System.out.println(n + " adalah bilangan genap");
+            
+        }else{
+                System.out.println(n + "adalah bilangan ganjil");
+            
+        }
+        
+    }
+}
