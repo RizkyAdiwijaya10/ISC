bfa1c1b90eb012169bbaad6ef7a99c7fe27cbd31
RizkyAdiwijaya10 <147222885+RizkyAdiwijaya10@users.noreply.github.com>
2023-11-22 00:03:07 +0800

    Add files via upload

---
+++ Day45.java
@@ -0,0 +1,11 @@
+public class Day45{
+
+    public static void main(String[] args) {
+        for(int r = 1; r <= 100; r+=2){
+            if(r == 45){
+                System.out.print("Day ");
+            }
+            System.out.println(r);
+        }
+    }
+}
