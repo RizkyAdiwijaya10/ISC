e1370d6a4405388f204d77880b8a004b3e44c502
RizkyAdiwijaya10 <147222885+RizkyAdiwijaya10@users.noreply.github.com>
2023-11-23 00:01:48 +0800

    Add files via upload

---
+++ Day46.java
@@ -0,0 +1,11 @@
+public class Day46{
+
+    public static void main(String[] args) {
+        int a = 7;
+        int b = a+2;
+        
+        for(int i = 0; i < b; i++){
+            System.out.println(++a);
+        }
+    }
+}
