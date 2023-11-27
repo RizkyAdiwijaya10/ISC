72bb14613251239f5378e7fdc85e13bf480e2e16
RizkyAdiwijaya10 <147222885+RizkyAdiwijaya10@users.noreply.github.com>
2023-11-27 21:54:10 +0800

    Add files via upload

---
+++ Day50.java
@@ -0,0 +1,28 @@
+public class Day50 {
+    public static void main(String[] args) {
+        //penjumlahan
+        int a = 1;
+        a += 10;
+        System.out.println("nilai a = " + a);
+        
+        //pengurangan
+        int b = 10;
+        b -= 3;
+        System.out.println("nilai b = " + b);
+        
+        //perkalian
+        int c = 5;
+        c *= 5;
+        System.out.println("nilai c = " + c);
+        
+        //pembagian
+        int d = 20;
+        d /= 2;
+        System.out.println("nilai d = " + d);
+        
+        //modulus
+        int e = 20;
+        e %= 6;
+        System.out.println("nilai e = " + e);
+    }
+}
