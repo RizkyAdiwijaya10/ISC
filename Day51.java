174d85c2262190141bdada0c86b91917d9d81bc1
RizkyAdiwijaya10 <147222885+RizkyAdiwijaya10@users.noreply.github.com>
2023-11-28 15:39:39 +0800

    Add files via upload

--- Day51.java
+++ Day51.java
@@ -1,26 +1,16 @@
-package informatic_study_club;
-
-
 public class Day51 {
     public static void main(String[] args) {
-        // unary + dan -
-        int a = 5;
-        System.out.println("nilai a = " + +a);
-        System.out.println("nilai a = " + -a);
+        int a,b;
+        boolean hasil;
         
-        // unary increment dan decrement
-        int angka = 10;
-        angka++;
-        System.out.println("nilai angka = " + angka);
-        int angka2 = 10;
-        angka2--;
-        System.out.println("nilai angka = " + angka2);
+        a = 10;
+        b = 10;
+        hasil = (a == b);
+        System.out.println("hasilnya adalah : " + hasil);
         
-        // unary negasi (!)
-        
-        boolean data = true;
-        System.out.println("nilai boolean = " + data);
-        System.out.println("nilai boolean = " + !data);
+        a = 10;
+        b = 10;
+        hasil = (a != b);
+        System.out.println("hasilnya adalah : " + hasil);
     }
- 
 }
