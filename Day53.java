83c1c0836b1cefea5e4f20acf030f1b83f601811
RizkyAdiwijaya10 <147222885+RizkyAdiwijaya10@users.noreply.github.com>
2023-11-30 00:14:08 +0800

    Add files via upload

---
+++ Day53.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+public class Day53 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("masukkan nilai a : ");
+        int a = sc.nextInt();
+        System.out.print("masukkan nilai b : ");
+        int b = sc.nextInt();
+        boolean hasil1 = (a == b || a != b);
+        System.out.println("apakah a sama == b? " + hasil1);
+        
+        System.out.print("masukkan nilai a : ");
+        int c = sc.nextInt();
+        System.out.print("masukkan nilai b : ");
+        int d = sc.nextInt();
+        boolean hasil2 = (c != d);
+        System.out.println("apakah a != b? " + hasil2);
+        
+        boolean total = a+b ==	c+d;
+        System.out.println("apakah a+b == c+d? " + total);
+    }
+}
