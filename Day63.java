ad4e095febae25b7cf2838d936f9b1fe65affa3a
RizkyAdiwijaya10 <147222885+RizkyAdiwijaya10@users.noreply.github.com>
2023-12-10 23:55:30 +0800

    Add files via upload

---
+++ Day63.java
@@ -0,0 +1,18 @@
+	import java.util.Scanner;
+
+public class Day63{
+    public static void main (String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("masukkan angka :");
+        int sb = sc.nextInt();
+        int sum = sb;
+        for(int i = sb; i > 0; i--){
+            System.out.println(i);
+             sum += i-1;
+        }   
+      
+        System.out.println(sum);
+        
+        
+    }
+}
