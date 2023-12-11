8dd5a6410bcd7da54a664d7d1a52302d3169addd
RizkyAdiwijaya10 <147222885+RizkyAdiwijaya10@users.noreply.github.com>
2023-12-11 22:47:42 +0800

    Add files via upload

---
+++ Day64.java
@@ -0,0 +1,15 @@
+public class Day64{
+
+    public static void main(String[] args) {
+        int n = 51;
+        
+        if(n % 2 == 0){
+                System.out.println(n + " adalah bilangan genap");
+            
+        }else{
+                System.out.println(n + " adalah bilangan ganjil");
+            
+        }
+        
+    }
+}
