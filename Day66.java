b118e8c3ea264617423f0206db2c4de037f91fbe
RizkyAdiwijaya10 <147222885+RizkyAdiwijaya10@users.noreply.github.com>
2023-12-14 00:15:37 +0800

    Add files via upload

---
+++ Day66.java
@@ -0,0 +1,8 @@
+public class Day66 {
+    public static void main(String[] args) {
+        String str1 = "Rizky ";
+        String str2 = "Adiwijaya";
+        String str3 = str1.concat(str2);
+        System.out.println(""+str3);
+    }
+}
