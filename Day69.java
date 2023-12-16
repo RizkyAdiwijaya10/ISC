d0c3253d33964322e59f72e7ab969974fc027da6
RizkyAdiwijaya10 <147222885+RizkyAdiwijaya10@users.noreply.github.com>
2023-12-16 22:53:19 +0800

    Add files via upload

---
+++ Day69.java
@@ -0,0 +1,14 @@
+public class Day69{
+    public static void main (String[] args){
+        int panjang,lebar;
+        panjang = 5;
+        lebar = 7;
+        
+        for(int i = 0; i < panjang; i++){
+            for(int j = 0; j < lebar; j++){
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
