5d3a749d8e76834e035925bf11b791c9e25c3aa7
RizkyAdiwijaya10 <147222885+RizkyAdiwijaya10@users.noreply.github.com>
2023-12-19 20:49:48 +0800

    Add files via upload

---
+++ Day72.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Day72 {
+
+    public static void main(String[] args) {
+        Scanner cs = new Scanner(System.in);
+        System.out.print("masukkan nilai : ");
+        int n = cs.nextInt();
+        for (int i = 0; i < n; i++) {
+            if (i > 0) {
+                System.out.print(",");
+            }
+            System.out.print(fibonacci(i));
+        }
+    }
+
+    public static int fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+}
