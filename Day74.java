2e2c46843bdd60c6153742020971d812e7c5402d
RizkyAdiwijaya10 <147222885+RizkyAdiwijaya10@users.noreply.github.com>
2023-12-21 23:31:17 +0800

    Add files via upload

---
+++ Day74.java
@@ -0,0 +1,12 @@
+public class Day74{
+    public static void main (String[] args){
+        int a,b;
+        boolean c;
+        
+        a = 5;
+        b = 8;
+        
+        c = a == b;
+        System.out.print("apakah a == b : "+c);
+    }
+}
