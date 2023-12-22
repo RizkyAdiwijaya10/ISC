c3e8aef03b0186f79f725b3b4ae8447100280a93
RizkyAdiwijaya10 <147222885+RizkyAdiwijaya10@users.noreply.github.com>
2023-12-22 23:21:24 +0800

    Add files via upload

---
+++ Day75.java
@@ -0,0 +1,14 @@
+public class Day75{
+    public static void main (String[] args){
+        int a,b,d;
+        boolean c;
+        
+        a = 5;
+        b = 8;
+        
+        c = a == b;
+        d = a * a;
+        System.out.print("apakah a == b : "+c);
+        System.out.print("\nHasil dari a * a = "+d);
+    }
+}
