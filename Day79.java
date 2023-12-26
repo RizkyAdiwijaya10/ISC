fab305402538f3f53ab71e0ab13f8ff2598416db
RizkyAdiwijaya10 <147222885+RizkyAdiwijaya10@users.noreply.github.com>
2023-12-26 21:39:24 +0800

    Add files via upload

---
+++ Day79.java
@@ -0,0 +1,17 @@
+public class Day79 {
+    public static void main(String[] args) {
+        // Nilai satuan dalam tipe data integer
+        int nilaiSatuan = 5;
+
+        // Konversi ke tipe data desimal (float)
+        float nilaiDesimalFloat = (float) nilaiSatuan;
+
+        // Konversi ke tipe data desimal (double)
+        double nilaiDesimalDouble = (double) nilaiSatuan;
+
+        // Menampilkan hasil konversi
+        System.out.println("Nilai Satuan (Integer): " + nilaiSatuan);
+        System.out.println("Nilai Desimal (Float): " + nilaiDesimalFloat);
+        System.out.println("Nilai Desimal (Double): " + nilaiDesimalDouble);
+    }
+}
