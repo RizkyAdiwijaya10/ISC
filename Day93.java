256bb4c5b98dcc3649c0b37c2597d9fea9aff3b9
RizkyAdiwijaya10 <147222885+RizkyAdiwijaya10@users.noreply.github.com>
2024-01-10 04:54:06 +0700

    Add files via upload

---
+++ Day93.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Day93{
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        //menggunakan method next
+        System.out.print("Input : ");
+        String data = sc.next();
+        System.out.print("Output : ");
+        System.out.print(data);
+        
+    }
+}
