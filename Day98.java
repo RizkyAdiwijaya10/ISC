bbcbd37e8ff126c40bbd5df55a3905d62fd3a37a
RizkyAdiwijaya10 <147222885+RizkyAdiwijaya10@users.noreply.github.com>
2024-01-13 19:40:39 +0700

    Add files via upload

---
+++ Day98.java
@@ -0,0 +1,11 @@
+public class Day98{
+
+    public static void main(String[] args) {
+        for(int r = 1; r <= 100; r++){
+            if(r == 98){
+                System.out.print("Day ");
+            }
+            System.out.println(r);
+        }
+    }
+}
